c722921438e7c3a73a266e807c1175f0b444abfc
FernandoLang <32783777+FernandoLang@users.noreply.github.com>
2021-05-10 19:32:11 -0300

    Add files via upload

---
+++ Ad_Manager/AdManager/DataBase/IDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdManager.DataBase
+{
+    public class IDbContext:DbContext
+    {
+        //Contexto do banco de dados, todas as classes adicionadas aqui serão mapeadas e uma tabela para cada
+        //será criada.
+        public IDbContext() : base("AdManager") { }
+
+        public DbContext create()
+        {
+            return new IDbContext();
+        }
+        public DbSet<Anuncio> anuncios { get; set; }
+        public DbSet<Clientes> clientes { get; set; }
+
+
+    }
+}
