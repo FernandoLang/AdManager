e31c68f66cfe61635ca48c3b77b6044cad875e80
FernandoLang <32783777+FernandoLang@users.noreply.github.com>
2021-05-10 19:31:29 -0300

    Add files via upload

---
+++ Ad_Manager/AdManager/Migrations/202105102147240_firstmigration.cs
@@ -0,0 +1,44 @@
+﻿namespace AdManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Anuncios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Client_Id = c.Int(nullable: false),
+                        Start_Date = c.DateTime(nullable: false),
+                        End_Date = c.DateTime(nullable: false),
+                        Value_per_day = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Clientes", t => t.Client_Id, cascadeDelete: true)
+                .Index(t => t.Client_Id);
+            
+            CreateTable(
+                "dbo.Clientes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Anuncios", "Client_Id", "dbo.Clientes");
+            DropIndex("dbo.Anuncios", new[] { "Client_Id" });
+            DropTable("dbo.Clientes");
+            DropTable("dbo.Anuncios");
+        }
+    }
+}
