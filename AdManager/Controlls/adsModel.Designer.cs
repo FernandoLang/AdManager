e0f7dc2ab8a4a60af06da71d5389362713f0dbb7
FernandoLang <32783777+FernandoLang@users.noreply.github.com>
2021-05-10 17:31:21 -0300

    Add files via upload

---
+++ AdManager/Controlls/adsModel.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace AdManager.Controlls
+{
+    partial class adsModel
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.adsName = new System.Windows.Forms.Label();
+            this.adsClient = new System.Windows.Forms.Label();
+            this.adsDateStart = new System.Windows.Forms.Label();
+            this.adsDateEnd = new System.Windows.Forms.Label();
+            this.lblid = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome   :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cliente :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(264, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Início      :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Termino :";
+            // 
+            // adsName
+            // 
+            this.adsName.AutoSize = true;
+            this.adsName.Location = new System.Drawing.Point(69, 9);
+            this.adsName.Name = "adsName";
+            this.adsName.Size = new System.Drawing.Size(44, 15);
+            this.adsName.TabIndex = 4;
+            this.adsName.Text = "Cliente";
+            // 
+            // adsClient
+            // 
+            this.adsClient.AutoSize = true;
+            this.adsClient.Location = new System.Drawing.Point(69, 27);
+            this.adsClient.Name = "adsClient";
+            this.adsClient.Size = new System.Drawing.Size(44, 15);
+            this.adsClient.TabIndex = 5;
+            this.adsClient.Text = "Cliente";
+            // 
+            // adsDateStart
+            // 
+            this.adsDateStart.AutoSize = true;
+            this.adsDateStart.Location = new System.Drawing.Point(326, 9);
+            this.adsDateStart.Name = "adsDateStart";
+            this.adsDateStart.Size = new System.Drawing.Size(44, 15);
+            this.adsDateStart.TabIndex = 6;
+            this.adsDateStart.Text = "Cliente";
+            // 
+            // adsDateEnd
+            // 
+            this.adsDateEnd.AutoSize = true;
+            this.adsDateEnd.Location = new System.Drawing.Point(326, 27);
+            this.adsDateEnd.Name = "adsDateEnd";
+            this.adsDateEnd.Size = new System.Drawing.Size(44, 15);
+            this.adsDateEnd.TabIndex = 7;
+            this.adsDateEnd.Text = "Cliente";
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(171, 27);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(17, 15);
+            this.lblid.TabIndex = 8;
+            this.lblid.Text = "id";
+            this.lblid.Visible = false;
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.Location = new System.Drawing.Point(326, 45);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(44, 15);
+            this.lblValue.TabIndex = 10;
+            this.lblValue.Text = "Cliente";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(231, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Valor Investido :";
+            // 
+            // adsModel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.Controls.Add(this.lblValue);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.adsDateEnd);
+            this.Controls.Add(this.adsDateStart);
+            this.Controls.Add(this.adsClient);
+            this.Controls.Add(this.adsName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "adsModel";
+            this.Size = new System.Drawing.Size(440, 62);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label adsName;
+        private System.Windows.Forms.Label adsClient;
+        private System.Windows.Forms.Label adsDateStart;
+        private System.Windows.Forms.Label adsDateEnd;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ AdManager/DataBase/Clientes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdManager.DataBase
+{
+    public class Clientes
+    {
+        //Classe responsável por compor a tabela Clientes do banco de dados
+        //Será com base nas propriedades dela que o programa irá gerar a tabela
+        [Key()]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        //Salva os dados preenchidos no banco de dados após as validações
+        public void Save()
+        {
+            IDbContext db = new IDbContext();
+            db.clientes.Add(this);
+            db.SaveChanges();
+        }
+        //Retorna todos os dados do banco de dados
+        public static List<Clientes> get_all()
+        {
+            IDbContext db = new IDbContext();
+            return db.clientes.ToList();
+        }
+
+
+    }
+}
